0acd5344f9445007854f25bb35a5f92f14cd1062
tifuseini <tifuseini@st.ug.edu.gh>
2022-08-04 20:27:02 +0000

    Update

--- src/main/java/com/tifuseini/tutorials/todomvcthymeleaf/TodoItemRepository.java
+++ src/main/java/com/tifuseini/tutorials/todomvcthymeleaf/TodoItemRepository.java
@@ -1,4 +1,8 @@
 package com.tifuseini.tutorials.todomvcthymeleaf;
 
-public class TodoItemRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TodoItemRepository extends JpaRepository<TodoItem, Long> {
+
 }
+
