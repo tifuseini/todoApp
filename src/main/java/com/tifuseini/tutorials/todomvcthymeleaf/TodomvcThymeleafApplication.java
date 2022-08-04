314a598306868bd61b1a773c300a31866820f16a
tifuseini <tifuseini@st.ug.edu.gh>
2022-08-04 20:21:46 +0000

    Update

---
+++ src/main/java/com/tifuseini/tutorials/todomvcthymeleaf/TodomvcThymeleafApplication.java
@@ -0,0 +1,13 @@
+package com.tifuseini.tutorials.todomvcthymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodomvcThymeleafApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodomvcThymeleafApplication.class, args);
+	}
+
+}
