95433f5260a1b7b2b0e9eca172addfcacc4cbf7d
tifuseini <tifuseini@st.ug.edu.gh>
2022-08-04 21:03:39 +0000

    Update

--- src/main/java/com/tifuseini/tutorials/todomvcthymeleaf/TodoItemFormData.java
+++ src/main/java/com/tifuseini/tutorials/todomvcthymeleaf/TodoItemFormData.java
@@ -1,4 +1,12 @@
 package com.tifuseini.tutorials.todomvcthymeleaf;
 
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+
+@Data
 public class TodoItemFormData {
+
+    @NotBlank
+    private String title;
 }
