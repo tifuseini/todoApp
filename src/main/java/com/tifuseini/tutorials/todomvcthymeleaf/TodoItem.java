a9de58d2a7891cf1a7af68305921dec3559cd1c0
tifuseini <tifuseini@st.ug.edu.gh>
2022-08-04 20:26:16 +0000

    Update

--- src/main/java/com/tifuseini/tutorials/todomvcthymeleaf/TodoItem.java
+++ src/main/java/com/tifuseini/tutorials/todomvcthymeleaf/TodoItem.java
@@ -1,4 +1,23 @@
 package com.tifuseini.tutorials.todomvcthymeleaf;
 
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Data
 public class TodoItem {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @NotBlank
+    private String title;
+
+    private boolean completed;
+
 }
