e54c2af2d117b5a66ee83e3656dd1b221fd854a6
tifuseini <tifuseini@st.ug.edu.gh>
2022-08-04 20:50:08 +0000

    Update

--- src/main/java/com/tifuseini/tutorials/todomvcthymeleaf/TodoItemcontroller.java
+++ src/main/java/com/tifuseini/tutorials/todomvcthymeleaf/TodoItemcontroller.java
@@ -1,13 +1,21 @@
 package com.tifuseini.tutorials.todomvcthymeleaf;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
+@AllArgsConstructor
 public class TodoItemcontroller {
 
-    private String index() {
+    private final TodoItemRepository todoItemRepository;
+
+    @GetMapping
+    private String index(Model model) {
+        model.addAttribute("totalNumberOfItems",todoItemRepository.count());
         return "index";
     }
 }
