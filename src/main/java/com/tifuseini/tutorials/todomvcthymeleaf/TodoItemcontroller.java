04b41aa8ad49788a09c7cd3d7dfc8db6957d83b5
tifuseini <tifuseini@st.ug.edu.gh>
2022-08-04 22:04:34 +0000

    Update

--- src/main/java/com/tifuseini/tutorials/todomvcthymeleaf/TodoItemcontroller.java
+++ src/main/java/com/tifuseini/tutorials/todomvcthymeleaf/TodoItemcontroller.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.validation.Valid;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/")
@@ -20,8 +22,21 @@ public class TodoItemcontroller {
     @GetMapping
     private String index(Model model) {
         model.addAttribute("item", new TodoItemFormData());
+        model.addAttribute("todos",getTodoItems());
         model.addAttribute("totalNumberOfItems",todoItemRepository.count());
         return "index";
+    }
+
+    private List<TodoItemDto> getTodoItems() {
+        return todoItemRepository.findAll()
+                .stream()
+                .map(todoItem -> new TodoItemDto(todoItem.getId(),
+                        todoItem.getTitle(),
+                        todoItem.isCompleted())
+                ).collect(Collectors.toList());
+    }
+
+    public static record TodoItemDto(long id, String title, boolean completed) {
     }
 
     @PostMapping
