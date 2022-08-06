617fa89adf301a8cd7dbf9527cac915bc4467154
tifuseini <tifuseini@st.ug.edu.gh>
2022-08-06 08:55:52 +0000

    Update

--- src/main/java/com/tifuseini/tutorials/todomvcthymeleaf/TodoItemRepository.java
+++ src/main/java/com/tifuseini/tutorials/todomvcthymeleaf/TodoItemRepository.java
@@ -4,5 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TodoItemRepository extends JpaRepository<TodoItem, Long> {
 
+    int countByCompleted(boolean completed);
+
 }
 
