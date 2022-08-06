0f43f40ea4d7aa4bf3f1c173e00f0f1d5151c5d7
tifuseini <tifuseini@st.ug.edu.gh>
2022-08-06 11:28:07 +0000

    Update

--- src/main/java/com/tifuseini/tutorials/todomvcthymeleaf/TodoItemcontroller.java
+++ src/main/java/com/tifuseini/tutorials/todomvcthymeleaf/TodoItemcontroller.java
@@ -17,25 +17,33 @@ public class TodoItemcontroller {
     private final TodoItemRepository todoItemRepository;
 
     @GetMapping
-    private String index(Model model) {
-        model.addAttribute("item", new TodoItemFormData());
-        model.addAttribute("todos",getTodoItems());
-        model.addAttribute("totalNumberOfItems",todoItemRepository.count());
-        model.addAttribute("numberOfActiveItems", todoItemRepository.countByCompleted(false));
+    public String index(Model model) {
+        addAttributesForIndex(model, ListFilter.ALL);
         return "index";
     }
 
-    private List<TodoItemDto> getTodoItems() {
-        return todoItemRepository.findAll()
-                .stream()
-                .map(todoItem -> new TodoItemDto(todoItem.getId(),
-                        todoItem.getTitle(),
-                        todoItem.isCompleted())
-                ).collect(Collectors.toList());
+    @GetMapping("/active")
+    public String indexActive(Model model) {
+        addAttributesForIndex(model, ListFilter.ACTIVE);
+        return "index";
     }
 
-    public static record TodoItemDto(long id, String title, boolean completed) {
+    @GetMapping("/completed")
+    public String indexCompleted(Model model) {
+        addAttributesForIndex(model, ListFilter.COMPLETED);
+        return "index";
     }
+
+    private void addAttributesForIndex(Model model, ListFilter listFilter) {
+        model.addAttribute("item", new TodoItemFormData());
+        model.addAttribute("filter", listFilter);
+        model.addAttribute("todos",getTodoItems(listFilter));
+        model.addAttribute("totalNumberOfItems",todoItemRepository.count());
+        model.addAttribute("numberOfActiveItems", getNumberOfActiveItems());
+        model.addAttribute("numberOfCompletedItems", getNumberOfCompletedItems());
+    }
+
+
 
     @PostMapping
     public String addTodoItem(@Valid @ModelAttribute("item") TodoItemFormData formData) {
@@ -52,9 +60,64 @@ public class TodoItemcontroller {
         return "redirect:/";
     }
 
+
+    @PutMapping("/toggle-all")
+    public String toggleAll() {
+        List<TodoItem> todoItems = todoItemRepository.findAll();
+        for (TodoItem todoItem : todoItems) {
+            todoItem.setCompleted(!todoItem.isCompleted());
+            todoItemRepository.save(todoItem);
+        }
+        return "redirect:/";
+    }
+
     @DeleteMapping("/{id}")
     public String deleteTodoItem(@PathVariable("id") long id) {
         todoItemRepository.deleteById(id);
         return "redirect:/";
+    }
+
+    @DeleteMapping("/completed")
+    public String deleteCompletedItems() {
+        List<TodoItem> items = todoItemRepository.findAllByCompleted(true);
+        for (TodoItem item : items) {
+            todoItemRepository.deleteById(item.getId());
+        }
+        return "redirect:/";
+    }
+
+
+    private List<TodoItemDto> getTodoItems(ListFilter filter) {
+        return switch (filter) {
+            case ALL -> convertToDto(todoItemRepository.findAll());
+            case ACTIVE -> convertToDto(todoItemRepository.findAllByCompleted(false));
+            case COMPLETED -> convertToDto(todoItemRepository.findAllByCompleted(true));
+        };
+    }
+
+    private List<TodoItemDto> convertToDto(List<TodoItem> todoItems) {
+        return todoItems
+                .stream()
+                .map(todoItem -> new TodoItemDto(todoItem.getId(),
+                        todoItem.getTitle(),
+                        todoItem.isCompleted()))
+                .collect(Collectors.toList());
+    }
+
+    private int getNumberOfActiveItems() {
+        return todoItemRepository.countAllByCompleted(false);
+    }
+
+    private int getNumberOfCompletedItems() {
+        return todoItemRepository.countAllByCompleted(true);
+    }
+
+    public static record TodoItemDto(long id, String title, boolean completed) {
+    }
+
+    public enum ListFilter {
+        ALL,
+        ACTIVE,
+        COMPLETED
     }
 }
